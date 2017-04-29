e8f2f432d2fad58c294d0eef962aaf47a9329bd9
taitai38 <noppawit.tai@mail.kmutt.ac.th>
2017-04-29 17:19:57 +0700

    showMenu collect branch

--- api.js
+++ api.js
@@ -11,7 +11,6 @@ var knex = require('knex')({
   }
 });
 
-var users = require('./user');
 var menu = require('./menu');
 
 const app = express();
@@ -28,14 +27,6 @@ app.get('/', function(req,res){
 
 app.get('/menu', function(req,res){
     res.json(menu.findAll());
-    // console.log("Request data:");
-    // console.log(req);
-    // console.log("Respond data:");
-    // console.log(res);
-});
-
-app.get('/user', function (req, res) {
-    res.json(users.findAll());
 });
 
 /* สั่งให้ server ทำการรัน Web Server ด้วย port ที่เรากำหนด */
