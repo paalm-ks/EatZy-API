c721687dc4da8c88d71866a7617248b6e955a820
PAALM <kawayuumi84@gmail.com>
2017-04-30 16:53:18 +0700

    menu.js delete console.log

--- menu.js
+++ menu.js
@@ -4,7 +4,6 @@ var data;
 
 // select query by mysql.config
 connect.select().from('Menu').then(function(a){
-            console.log(a);
             data=a;                           // add a to data
 });
 
@@ -12,20 +11,3 @@ connect.select().from('Menu').then(function(a){
 exports.findAll = function() {
         return data ;
 }
-
-
-/* raw data shouldd be 
- var b = [ 
-             {  menuNo: 5,
-                 menuNameTH: 'กะเพราไก่',
-                 menuNameEN: 'Kaprao Kai',
-                 menuDesc: 'B2',
-                 menuPrice: 45,
-                 isOfficialMenu: 0 },
-             {  menuNo: 6,
-                 menuNameTH: 'ผัดไทยกุ้งสด',
-                 menuNameEN: 'Pud Thai',
-                menuDesc: 'เส้นใหญ่อร่อยมากกกกก',
-                 menuPrice: 35,
-                 isOfficialMenu: 0 } ];
-*/
