8fef351ba11ba5d6a05f6221f2630ff2cc140e41
PAALM <kawayuumi84@gmail.com>
2017-10-10 22:37:11 +0700

    edit selected data in showmyorder

--- myOrder.js
+++ myOrder.js
@@ -2,18 +2,18 @@ var knex = require('./knex');
 var bill = require('./bill');
 
 const services = {
-    getMyOrder: (billNo) => {
-            return knex.select('Order.orderNo','Menu.menuNameTH','Order.quantity','Order.amount').from('Bill')
-            .join('Order', 'Bill.billNo', 'Order.billNo')  
-            .join('Menu','Menu.menuNo','Order.menuNo') 
-            .where('Bill.billNo', billNo)     
-    },
-    getMyAddon: (orderNo) => {
-        return knex.select('Material.matName','Addon.price').from('Addon')
-        .join('Order_Addon','Addon.addOnNo','Order_Addon.addOnNo')
-        .join('Material','Addon.matNo','Material.matNo' )
-        .where('Order_Addon.orderNo', orderNo)    
-}
+        getMyOrder: (billNo) => {
+                return knex.select('Order.orderNo', 'Menu.menuNo', 'Menu.menuPrice', 'Menu.menuPicPath', 'Menu.menuNameTH', 'Order.quantity', 'Order.amount').from('Bill')
+                        .join('Order', 'Bill.billNo', 'Order.billNo')
+                        .join('Menu', 'Menu.menuNo', 'Order.menuNo')
+                        .where('Bill.billNo', billNo)
+        },
+        getMyAddon: (orderNo) => {
+                return knex.select('Material.matName', 'Addon.price').from('Addon')
+                        .join('Order_Addon', 'Addon.addOnNo', 'Order_Addon.addOnNo')
+                        .join('Material', 'Addon.matNo', 'Material.matNo')
+                        .where('Order_Addon.orderNo', orderNo)
+        }
 }
 
 exports.showMyOrder = async (billNo) => {
@@ -21,42 +21,42 @@ exports.showMyOrder = async (billNo) => {
                 const billDetail = await bill.getBillByNo(billNo);
                 const order = await services.getMyOrder(billNo);
                 const addonss = [];
-                for(i in order){
-                console.log(order[i].orderNo);
-                const addon = await services.getMyAddon(order[i].orderNo);
-                console.log(addon)
-                addonss.push(addon) ;
+                for (i in order) {
+                        console.log(order[i].orderNo);
+                        const addon = await services.getMyAddon(order[i].orderNo);
+                        console.log(addon)
+                        addonss.push(addon);
                 }
                 console.log(addonss)
-                return [{ 
-                    bill : billDetail ,
-                    orders  : order ,
-                    addons : addonss
-                }] 
+                return [{
+                        bill: billDetail,
+                        orders: order,
+                        addons: addonss
+                }]
         } catch (err) {
                 console.log(err)
         }
 }
 
 exports.showUserOrder = async (userNo) => {
-        try {   
+        try {
                 const billNoArr = await bill.showBill(userNo);
                 const billNo = billNoArr[0].billNo;
                 const billDetail = await bill.getBillByNo(billNo);
                 const order = await services.getMyOrder(billNo);
                 const addonss = [];
-                for(i in order){
-                console.log(order[i].orderNo);
-                const addon = await services.getMyAddon(order[i].orderNo);
-                console.log(addon)
-                addonss.push(addon) ;
+                for (i in order) {
+                        console.log(order[i].orderNo);
+                        const addon = await services.getMyAddon(order[i].orderNo);
+                        console.log(addon)
+                        addonss.push(addon);
                 }
                 console.log(addonss)
-                return [{ 
-                    bill : billDetail ,
-                    orders  : order ,
-                    addons : addonss
-                }] 
+                return [{
+                        bill: billDetail,
+                        orders: order,
+                        addons: addonss
+                }]
         } catch (err) {
                 console.log(err)
         }
